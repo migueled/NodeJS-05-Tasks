b30456ae4145385243e31c04adfaf599533d4e5a
MiguelED <migueled2695@gmail.com>
2021-05-04 02:30:27 -0500

    Get key

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const mongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-mongoClient.connect( connectionURL , { useNewUrlParser : true , useUnifiedTopology: true } , ( error , client ) => {
+mongoClient.connect( connectionURL , { useNewUrlParser : true , useUnifiedTopology: true } , ( error , client ) => {//CALLBACK
     if( error ) {
         return console.log( 'Unable to connect to database' );
     }
     const db = client.db( databaseName )
 
     db.collection( 'users' ).insertOne({
-        name : 'Miguel',
+        name : 'Eduardo',
         age : 25
+    } , ( error , result ) => {
+        if( error ) {
+            return console.log( 'Unable to insert user' )
+        }
+        console.log( result.ops )
     })
-    console.log( 'Success' );
+    
 })
