34b15bf9ce1666e9461f7ebff4756c8f50163dfc
MiguelED <migueled2695@gmail.com>
2021-05-07 01:12:14 -0500

    Search query

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-/*const mongodb = require( 'mongodb' )
-const mongoClient = mongodb.MongoClient
-const objectId = mongodb.ObjectId => */
-
 const { MongoClient , ObjectId } = require( 'mongodb' )
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
-
-const id = new ObjectId()
-console.log( id )
-console.log( id.id )
-console.log( id.id.length )
-console.log( id.toHexString() )
-console.log( id.toHexString().length )
-console.log( id.getTimestamp() )
 
 MongoClient.connect( connectionURL , { useNewUrlParser : true , useUnifiedTopology: true } , ( error , client ) => {//CALLBACK
     if( error ) {
@@ -21,51 +9,8 @@ MongoClient.connect( connectionURL , { useNewUrlParser : true , useUnifiedTopolo
     }
     const db = client.db( databaseName )
 
-    /*db.collection( 'users' ).insertOne({
-        _id : id,
-        name : 'Pablo',
-        age : 25
-    } , ( error , result ) => {
-        if( error ) {
-            return console.log( 'Unable to insert user' )
-        }
-        console.log( result.ops )
-    })*/
-
-    /*db.collection( 'users' ).insertMany([
-        {
-            name : 'Jen',
-            age : 28
-        },
-        {
-            name : 'Gunter',
-            age : 27
-        }
-    ] , ( error , result ) => {
-        if( error ) {
-            return console.log( 'Unable to inser documents!' )
-        }
-        console.log( result.ops );
-    })*/
-
-    /*db.collection( 'task' ).insertMany([
-        {
-            description : 'First task',
-            completed : true
-        },
-        {
-            description : 'Example of task',
-            completed : true
-        },
-        {
-            description : 'Buy weather',
-            completed : false
-        }
-    ] , ( error , result ) => {
-        if( error ) {
-            return console.log( 'Unable to insert tasks!' )
-        }
-        console.log( result.ops );
-    })*/
-    
+    db.collection('users').findOne( { name : 'Miguel' } , ( error , data ) => {
+        if ( error ) { return console.log( 'unable to fetch' ) }
+        console.log( data )
+    })
 })
