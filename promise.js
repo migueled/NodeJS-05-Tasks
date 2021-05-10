f5c81f38fe08e61063177867db012a76d404d1e1
MiguelED <migueled2695@gmail.com>
2021-05-10 00:06:21 -0500

    Promise chaining

--- promise.js
+++ promise.js
@@ -6,6 +6,26 @@ const add = ( a , b ) => {
     })
 }
 
-add( 1 , 5)
-    .then( sum => console.log( sum ) )
+/*add( 1 , 5)
+    .then( sum => {
+        console.log( sum )
+        add( sum , 5 )
+            .then( suma2 => console.log( suma2 ) )
+            .catch( error => console.log( error ) )
+    })
+    .catch( error => console.log( error ) )*/
+    /*
+    No es la forma correcta de usar esta opcion
+    Cuanto ma´s tareas asíncronas intentamos realizar,
+    más codificado y complejo es nuestro codigo.
+        - Código duplicado para los errores
+
+    -Hay una manera de hacerlo mejor usando Promise chaining
+    */
+
+add( 1 , 2 )
+    .then( suma => {
+        console.log( suma )
+        return add( suma , 5 )
+    }).then( suma2 => console.log( suma2) )
     .catch( error => console.log( error ) )
