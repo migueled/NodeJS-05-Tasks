5040aa09b5093617267f1605a7397f3516131894
MiguelED <migueled2695@gmail.com>
2021-05-13 16:18:06 -0500

    Promise chaining countDocuments

--- promise-chaining.js
+++ promise-chaining.js
@@ -1,10 +1,17 @@
 require( './src/database/mongoose')
 const User = require( './src/models/user')
-
-User.findByIdAndUpdate( '60987b5b7b8d690cac033013' , { age : 1 } )
+const Task = require( './src/models/task')
+/*User.findByIdAndUpdate( '60987b5b7b8d690cac033013' , { age : 1 } )
     .then( user => {
         console.log( user )
         return User.countDocuments( { age : 1 })
     }).then( result => {
         console.log( result )
-    }).catch( error => console.log( error ) )
+    }).catch( error => console.log( error ) )*/
+
+Task.findByIdAndDelete( '609761e05f9a2213d01bd576' )
+    .then( () => {
+        console.log( 'Task remove' )
+        return Task.find( { completed : false } ).countDocuments()
+    }).then( result => console.log( result ) )
+    .catch( error => console.log( error) )
