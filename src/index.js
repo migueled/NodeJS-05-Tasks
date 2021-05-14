d54420640e3ac7380fee2e9183f9e89e4a80f84c
MiguelED <migueled2695@gmail.com>
2021-05-14 00:10:41 -0500

    Resource updating endpoint

--- src/index.js
+++ src/index.js
@@ -44,6 +44,24 @@ app.get( '/users/:id' , async ( req , res ) => {
     }
 })
 
+app.patch( '/users/:id' , async ( req , res ) => {
+    const updates = Object.keys( req.body )
+    const allowUpdates = [ 'name' , 'email' , 'password' , 'age' ]
+    const isValidOperation = updates.every( update => allowUpdates.includes( update ) )
+    if( !isValidOperation ) {
+        return res.status( 400 ).send( { error : 'Invalid updates!' } )
+    }
+    try {
+        const user = await User.findByIdAndUpdate( req.params.id , req.body , { new : true , runValidators : true } )
+        if( !user ) {
+            res.status( 404 ).send()
+        }
+        res.send( user )
+    } catch (error) {
+        res.status( 400 ).send( error )
+    }
+})
+//--------
 app.post( '/tasks' , async ( req , res ) => {
     const newTask = new Task( req.body )
     try {
