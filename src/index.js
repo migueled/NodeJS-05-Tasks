ed394b0ea5ff81828c4e0504c7712e7edff8b840
MiguelED <migueled2695@gmail.com>
2021-05-23 14:03:03 -0500

    Json web tokens

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ const port = 3000
 app.use( express.json() )//For parsing application/JSON
 app.use( userRouter )
 app.use( taskRouter )
+
+const jwt = require( 'jsonwebtoken' )
+const myFunction = async () => {
+    const token = jwt.sign( { _id : 'abc123' } , 'ThisIsMyNewCourse' , { expiresIn : '7 days' } )
+    console.log( token )
+    const data = jwt.verify( token , 'ThisIsMyNewCourse' )
+    console.log( data )
+}
+myFunction()
 /******/
 app.listen( port , () => {
     console.log( `Server is up on port ${ port }`);
