9e0b886c9e8b3c1930c9dc05fd832391fc258f31
MiguelED <migueled2695@gmail.com>
2021-05-31 22:12:11 -0500

    Refactor GET/Tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,10 +17,12 @@ router.post( '/tasks' , auth , async ( req , res ) => {
     }
 })
 
-router.get( '/tasks' , async ( req , res ) => {
+router.get( '/tasks' , auth , async ( req , res ) => {
     try {
-        const tasks = await Task.find({})
-        res.status( 200 ).send( tasks )
+        //const tasks = await Task.find({ owner : req.user._id })
+        //res.status( 200 ).send( tasks )
+        await req.user.populate('tasks').execPopulate()
+        res.status( 200 ).send( req.user.tasks )
     } catch (error) {
         res.status( 500 ).send( error )
     }
