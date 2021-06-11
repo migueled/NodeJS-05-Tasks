52927376b7d25272ecac26076fc2036c3ce37c4b
MiguelED <migueled2695@gmail.com>
2021-06-10 21:39:58 -0500

    paginating data

--- src/routers/task.js
+++ src/routers/task.js
@@ -30,7 +30,8 @@ router.get( '/tasks' , auth , async ( req , res ) => {
             path : 'tasks',
             match,
             options : {
-                limit : parseInt( req.query.limit )
+                limit : parseInt( req.query.limit ),
+                skip : parseInt( req.query.skip )
             }
         }).execPopulate()
 
