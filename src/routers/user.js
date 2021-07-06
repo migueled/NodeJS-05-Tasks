a8de907795978a9c78dd0521466495ebb692b9df
MiguelED <migueled2695@gmail.com>
2021-07-06 03:28:10 -0500

    npm i sharp

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require( 'express' )
 const router = new express.Router()
 const multer = require( 'multer' )
+const sharp = require( 'sharp' )
 
 const User = require( '../models/user' )
 const auth = require( '../middleware/auth')
