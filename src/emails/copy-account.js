dbdaf237d7759decfdfa5e69993a2ea1ee40a202
MiguelED <migueled2695@gmail.com>
2021-07-10 02:25:50 -0500

    Exploring SendGrid

---
+++ src/emails/copy-account.js
@@ -0,0 +1,16 @@
+const sgMail = require('@sendgrid/mail')
+const SENDGRID_API_KEY=''
+
+sgMail.setApiKey(  SENDGRID_API_KEY )
+
+const msg = {
+    to: '', // Change to your recipient
+    from: '', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+
+sgMail.send(msg)
+  .then( () => console.log('Email sent') )
+  .catch( (error) => console.error(error) )
